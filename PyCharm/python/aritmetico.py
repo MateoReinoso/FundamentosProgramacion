4a5d317950405c734b7e95590b49bb5fd48efb32
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-04-21 23:02:36 -0500

    operadores aritmeticos

---
+++ PyCharm/python/aritmetico.py
@@ -0,0 +1,16 @@
+print("ingresa el primer valor")
+num1 = int(input())
+print("ingresa el segundo valor")
+num2 = int(input())
+resultado = num1 + num2
+print("resultado = num1 + num2")
+print("El resultado ",resultado)
+resultado = num1 * num2
+print("resultado = num1 * num2")
+print("El resultado ",resultado)
+resultado = num1 - num2
+print("resultado = num1 - num2")
+print("El resultado ",resultado)
+resultado = num1 / num2
+print("resultado = num1 / num2")
+print("El resultado ",resultado)
