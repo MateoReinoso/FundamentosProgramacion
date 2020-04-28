f7441ddd0b2ef337dba2f3567f469e4b592a11ba
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-04-27 20:34:48 -0500

    Fundamentos de Programacion Nivel 2

---
+++ Fundamentos2/funciones.py
@@ -0,0 +1,5 @@
+def salaryWeek(hrs, salaryHrs, job):
+    salary = hrs * salaryHrs
+    salary = salary * 7
+    print("El sueldo de ", job,"es",salary)
+
---
+++ Fundamentos2/main.py
@@ -0,0 +1,6 @@
+import funciones
+print("Ingrese las horas trabajadas, el salario de la semana y la profesion a que se dedica")
+hrs = int(input())
+salaryWeek = int(input())
+job = input()
+print(funciones.salaryWeek(hrs, salaryWeek, job))
