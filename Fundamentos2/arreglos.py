fdce436f05fc52a69cd82d8b531c6b485969c7a1
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-04-30 17:47:26 -0500

    Recorrer Arreglos

--- Fundamentos2/arreglos.py
+++ Fundamentos2/arreglos.py
@@ -1,6 +1,14 @@
 #un arreglo es una lista en python
 colores = ["Verde", "Amarillo", "Morado"]
+persona = {"name":"Daniel",
+           "lastNAme":"Reinoso",
+           "age":22}
 
-persona = {"name": "Daniel","lastNAme": "Reinoso","age": "24"}
 
-print(persona.get("age"))
+#de esta forma recorremos el arreglo
+for value in colores:
+    print(value)
+
+#de esta forma nosotros recorremos el valor de un diccionario imprimiendo todos sus itemes
+for key, value in persona.items():
+    print(key,value)
---
+++ Fundamentos2VC/arreglos.py
@@ -0,0 +1,8 @@
+person = {
+    "name":"Daniel",
+    "lastname":"Reinoso",
+    "age":24
+}
+
+for value in person:
+    print(value)
